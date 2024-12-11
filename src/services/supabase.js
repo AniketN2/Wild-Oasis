af05745c7617e81fe2ee45b428b0b3e238b79240
AniketN2 <121167260+AniketN2@users.noreply.github.com>
2024-12-11 22:18:19 +0530

    integrating supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://fhwnehlsxqhsmlcuwxdb.supabase.co';
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZod25laGxzeHFoc21sY3V3eGRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM5Mjk5MzIsImV4cCI6MjA0OTUwNTkzMn0.tdXqWPGSxNqG2euboMXxkT6CyaCezbTsm7mmYHvZXhA";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
