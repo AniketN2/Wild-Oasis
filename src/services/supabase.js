83b5cf7dc115f894515e8ea4b0d9ebaf2a94df64
AniketN2 <121167260+AniketN2@users.noreply.github.com>
2024-12-14 20:56:01 +0530

    Final Commit
    Done all the changes and finally after a intense hardword the final code is development and onto deployment now

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://fhwnehlsxqhsmlcuwxdb.supabase.co';
+export const supabaseUrl = 'https://fhwnehlsxqhsmlcuwxdb.supabase.co';
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZod25laGxzeHFoc21sY3V3eGRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM5Mjk5MzIsImV4cCI6MjA0OTUwNTkzMn0.tdXqWPGSxNqG2euboMXxkT6CyaCezbTsm7mmYHvZXhA";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
